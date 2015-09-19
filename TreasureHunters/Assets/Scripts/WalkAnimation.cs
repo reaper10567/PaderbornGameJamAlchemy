ef54fe6be4b3ba63b1198924459763f391d58b7f
Jan T <janhentie@web.de>
2015-09-19 16:50:28 -0400

    walk animation

---
+++ TreasureHunters/Assets/Scripts/WalkAnimation.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class WalkAnimation : MonoBehaviour {
+
+    public List<Sprite> North;
+    public List<Sprite> West;
+    public List<Sprite> East;
+    public List<Sprite> South;
+    List<Sprite>.Enumerator ne;
+    List<Sprite>.Enumerator we;
+    List<Sprite>.Enumerator ee;
+    List<Sprite>.Enumerator se;
+
+    void Start()
+    {
+        ne = North.GetEnumerator();
+        we = West.GetEnumerator();
+        ee = East.GetEnumerator();
+        se = South.GetEnumerator();
+    }
+
+    public void moveNorth()
+    {
+        if (!ne.MoveNext())
+        {
+            ne = North.GetEnumerator();
+            ne.MoveNext();
+        }
+        GetComponent<SpriteRenderer>().sprite = ne.Current;
+    }
+
+    public void moveWest()
+    {
+        if (!we.MoveNext())
+        {
+            we = West.GetEnumerator();
+            we.MoveNext();
+        }
+        GetComponent<SpriteRenderer>().sprite = we.Current;
+    }
+
+    public void moveEast()
+    {
+        if (!ee.MoveNext())
+        {
+            ee = East.GetEnumerator();
+            ee.MoveNext();
+        }
+        GetComponent<SpriteRenderer>().sprite = ee.Current;
+    }
+
+    public void moveSouth()
+    {
+        if (!se.MoveNext())
+        {
+            se = South.GetEnumerator();
+            se.MoveNext();
+        }
+        GetComponent<SpriteRenderer>().sprite = se.Current;
+    }
+}
